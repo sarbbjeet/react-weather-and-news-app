13d00dbd55763a7a7c70571ebeb4afd69e77e512
Sarbjit Singh <sarbjeets18@gmail.com>
2021-09-20 10:41:39 +0100

    access store using StoreContext method(simple way without using react-redux lib)

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,27 @@ import NavBar from "./components/common/NavBar";
 import SearchByCity from "./components/searchByCity";
 import News from "./components/News";
 import WeatherDashboard from "./components/weatherDashboard";
+import StoreContext from "./contexts/storeContext";
+import store from "./store/configureStore";
 
 function App() {
   return (
-    <div className="bg-light" style={{ height: "100vh" }}>
-      <NavBar />
-      <Container>
-        <Switch>
-          <Route exact path="/weather" component={SearchByCity}></Route>
-          <Route path="/news" component={News}></Route>
-          <Route path="/weather/dashboard" component={WeatherDashboard}></Route>
-        </Switch>
-      </Container>
-    </div>
+    // pass store to every component
+    <StoreContext.Provider value={store}>
+      <div className="bg-light" style={{ height: "100vh" }}>
+        <NavBar />
+        <Container>
+          <Switch>
+            <Route exact path="/weather" component={SearchByCity}></Route>
+            <Route path="/news" component={News}></Route>
+            <Route
+              path="/weather/dashboard"
+              component={WeatherDashboard}
+            ></Route>
+          </Switch>
+        </Container>
+      </div>
+    </StoreContext.Provider>
   );
 }
 
--- src/components/weatherDashboard.jsx
+++ src/components/weatherDashboard.jsx
@@ -1,36 +1,39 @@
-import React, { useEffect } from "react";
+import React, { Component, useEffect } from "react";
+import StoreContext from "../contexts/storeContext";
+import { loadApi } from "../store/weather";
 import * as images from "../utils/images";
 import "./common/form.css";
 import WeatherIcon from "./common/weatherIcon";
-import { loadApi } from "../store/weather";
-import store from "../store/configureStore";
-import {
-  apiCallSuccess,
-  apiCallRequest,
-  apiCallFailed,
-} from "../store/actionCreater";
 
-export default function WeatherDashboard(props) {
-  const { addr, temp, wind, press, pre } = props;
-  useEffect(() => {
-    // store.dispatch({ type: "weather/apiLoaded", payload: { name: "sinhg" } });
+export default class WeatherDashboard extends Component {
+  static contextType = StoreContext;
+  state = {
+    weather: {},
+  };
+  componentDidMount() {
+    const store = this.context;
+    this.unsubscribe = store.subscribe(() => {
+      this.setState({ weather: store.getState().entities.weather.data });
+    });
     store.dispatch(loadApi("middlesbrough"));
-    //store.dispatch(apiCallFailed({ singh: "/dashboad" }));
-    // store.dispatch(apiCallSuccess({ name: "singh" }));
-  }, []);
-
-  return (
-    <div className="formContainer mt-3">
-      <h3 style={{ alignSelf: "center" }}>{addr}</h3>
-      <div className="mt-3 weatherContainer">
-        <WeatherIcon label="Sunny" image={images.Sunny} />
-        <h2>24C</h2>
-        <div>
-          <p>Wind: 13 kwph</p>
-          <p>Precip: 0 mm</p>
-          <p>Pressure: 1022 mb</p>
+  }
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  render() {
+    return (
+      <div className="formContainer mt-3">
+        <h3 style={{ alignSelf: "center" }}>{this.state.weather.name}</h3>
+        <div className="mt-3 weatherContainer">
+          <WeatherIcon label="Sunny" image={images.Sunny} />
+          <h2>24C</h2>
+          <div>
+            <p>Wind: 13 kwph</p>
+            <p>Precip: 0 mm</p>
+            <p>Pressure: 1022 mb</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
