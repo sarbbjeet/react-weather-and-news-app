0c70a46f8a8bfcd3be281ce56bc007307efc5faf
Sarbjit Singh <sarbjeets18@gmail.com>
2021-10-04 10:07:19 +0100

    news-details page redirect error fixed

--- src/components/detailedNews.jsx
+++ src/components/detailedNews.jsx
@@ -3,26 +3,30 @@ import { Card } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 export default function DetailedNews(props) {
+  const { state } = props.history.location;
   const allNews = useSelector((state) => state.entities.news.data.articles);
 
   return (
     <>
+      {allNews && (
+        <Card
+          className="mt-3"
+          style={{
+            width: "40rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            overflow: "auto",
+          }}
+        >
+          <Card.Body>
+            <Card.Title>{allNews[1].title}</Card.Title>
+            <Card.Img src={allNews[1].urlToImage} />
+            <Card.Text className="mt-3">{allNews[1].content}</Card.Text>
+          </Card.Body>
+        </Card>
+      )}
       {!allNews && props.history.push("/news")}
-      <Card
-        className="mt-3"
-        style={{
-          width: "40rem",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Card.Body>
-          <Card.Title>{allNews[0].title}</Card.Title>
-          <Card.Img src={allNews[0].urlToImage} />
-          <Card.Text className="mt-3">{allNews[0].content}</Card.Text>
-        </Card.Body>
-      </Card>
     </>
   );
 }
